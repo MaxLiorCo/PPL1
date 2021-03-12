26bd5fd45fb0f473db73b752c397ca1162526d52
khtur9512300 <khtur9512300@walla.com>
2021-03-12 20:37:11 +0200

    stack finish

--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -2,8 +2,13 @@ import { State, bind } from "./state";
 
 export type Stack = number[];
 
-export const push = undefined;
+export const push: (x:number) => State<Stack,number> =
+(x)=> (stack)=> [[x].concat(stack),x] //[[<->x,1,2,3],x]
 
-export const pop = undefined;
 
-export const stackManip = undefined;
+export const pop: State<Stack,number> = (stack) => [stack.slice(1), stack[0]]
+
+export const stackManip: State<Stack,number> = 
+bind(pop, x => bind(push(x*x), y=> bind(pop, z=> push(x+y)) ) )
+
+console.log(stackManip([4,5,6]))
