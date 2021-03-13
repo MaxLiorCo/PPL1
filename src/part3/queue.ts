8c6197bd51af1d6a79e5d939e3200539e40b1767
khtur9512300 <khtur9512300@walla.com>
2021-03-13 10:14:37 +0200

    done!

--- src/part3/queue.ts
+++ src/part3/queue.ts
@@ -3,9 +3,9 @@ import { State, bind } from "./state";
 export type Queue = number[];
 
 
-export const enqueue: (x:number) => State<Queue,number> =
-     (x)=> (queue)=> [queue.concat(x),x] //[[1,2,3,x],x]
+export const enqueue: (x:number) => State<Queue,undefined> =
+     (x)=> (queue)=> [queue.concat(x), undefined] //[[<-1,2,3,x<-],undefined]
 
 export const dequeue: State<Queue,number> = (queue) => [queue.slice(1), queue[0]]
 export const queueManip: State<Queue,number> = 
-bind(dequeue, x => bind(enqueue(x*2), y=> bind(enqueue(y/6), z=> dequeue) ) )
+bind(dequeue, x => bind(enqueue(x*2), y=> bind(enqueue(x/3), z=> dequeue) ) )
--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -2,13 +2,11 @@ import { State, bind } from "./state";
 
 export type Stack = number[];
 
-export const push: (x:number) => State<Stack,number> =
-(x)=> (stack)=> [[x].concat(stack),x] //[[<->x,1,2,3],x]
+export const push: (x:number) => State<Stack,undefined> =
+(x)=> (stack)=> [[x].concat(stack),undefined] //[[<->x,1,2,3],x]
 
 
 export const pop: State<Stack,number> = (stack) => [stack.slice(1), stack[0]]
 
-export const stackManip: State<Stack,number> = 
-bind(pop, x => bind(push(x*x), y=> bind(pop, z=> push(x+y)) ) )
-
-console.log(stackManip([4,5,6]))
+export const stackManip: State<Stack,undefined> = 
+bind(pop, x => bind(push(x*x), y=> bind(pop, z=> push(x+z)) ) )
