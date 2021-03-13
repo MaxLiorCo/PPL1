7b1bac64e20e7ea417e66b14dd4e41bb496984a1
madMax342 <maximgel@post.bgu.ac.il>
2021-03-13 11:45:16 +0200

    Update stack.ts

--- src/part3/stack.ts
+++ src/part3/stack.ts
@@ -3,7 +3,7 @@ import { State, bind } from "./state";
 export type Stack = number[];
 
 export const push: (x:number) => State<Stack,undefined> =
-(x)=> (stack)=> [[x].concat(stack),undefined] //[[<->x,1,2,3],x]
+(x)=> (stack)=> [[x].concat(stack),undefined] //[[<->x,1,2,3],undefined]
 
 
 export const pop: State<Stack,number> = (stack) => [stack.slice(1), stack[0]]
